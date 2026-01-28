8622bdf45894b2c55e456bdff27bcafd25ef1eb6
llLeo306 <li13780602203@126.com>
2026-01-28 14:44:31 +0800

    remote ci

--- Motor.hpp
+++ Motor.hpp
@@ -1,13 +1,4 @@
 #pragma once
-
-// clang-format off
-/* === MODULE MANIFEST V2 ===
-module_description: No description provided
-constructor_args: []
-template_args: []
-required_hardware: []
-depends: []
-=== END MANIFEST === */
 // clang-format on
 
 #include <cstdint>
